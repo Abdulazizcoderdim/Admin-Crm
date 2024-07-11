365efad11bed96e896d1b46d0d7b67fdf0d4b646
AbdulazizRus <coderdim@gmail.com>
2024-07-11 18:25:43 +0500

    changes

--- app/ui/dashboard/sidebar/sidebar.jsx
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -1,5 +1,93 @@
+import styles from './sidebar.module.css'
+import {
+  MdAnalytics,
+  MdAttachMoney,
+  MdDashboard,
+  MdHelpCenter,
+  MdOutlineSettings,
+  MdPeople,
+  MdShoppingBag,
+  MdSupervisedUserCircle,
+  MdWork,
+} from 'react-icons/md'
+import MenuLink from "./menuLink/menuLink";
+
+const menuItems = [
+  {
+    title: 'Pages',
+    list: [
+      {
+        title: 'Dashboard',
+        path: '/dashboard',
+        icon: <MdDashboard />,
+      },
+      {
+        title: 'Users',
+        path: '/dashboard/users',
+        icon: <MdSupervisedUserCircle />,
+      },
+      {
+        title: 'Products',
+        path: '/dashboard/products',
+        icon: <MdShoppingBag />,
+      },
+      {
+        title: 'Transactions',
+        path: '/dashboard/transactions',
+        icon: <MdAttachMoney />,
+      },
+    ],
+  },
+  {
+    title: 'Analytics',
+    list: [
+      {
+        title: 'Revenue',
+        path: '/dashboard/revenue',
+        icon: <MdWork />,
+      },
+      {
+        title: 'Reports',
+        path: '/dashboard/reports',
+        icon: <MdAnalytics />,
+      },
+      {
+        title: 'Teams',
+        path: '/dashboard/teams',
+        icon: <MdPeople />,
+      },
+    ],
+  },
+  {
+    title: 'User',
+    list: [
+      {
+        title: 'Settings',
+        path: '/dashboard/settings',
+        icon: <MdOutlineSettings />,
+      },
+      {
+        title: 'Help',
+        path: '/dashboard/help',
+        icon: <MdHelpCenter />,
+      },
+    ],
+  },
+]
+
 const Sidebar = () => {
-  return <div>Sidebar</div>
+  return <div className={styles.container}>
+    <ul>
+      {menuItems.map((cat) => (
+        <li key={cat.title}>
+          <span className={styles.cat}>{cat.title}</span>
+          {cat.list.map((item) => (
+            <MenuLink item={item} key={item.title}/>
+          ))}
+        </li>
+      ))}
+    </ul>
+  </div>
 }
 
 export default Sidebar
